123341e067ed4920a6c0811b5ec72c1d9aae5b80
Tahsin Maria <mariatahsin3.1416@gmail.com>
2026-09-02 17:21:48 +0600

    updated file

--- Javascript Course/Module - 03/Day - 06 array methods part - 02.js
+++ Javascript Course/Module - 03/Day - 06 array methods part - 02.js
@@ -1,4 +1,22 @@
+//  {#3e8,7}
 /*
 find() → find the FIRST matching element.
 filter() → find ALL matching elements.
+
+some() → Does at least ONE element satisfy the condition?
+every() → Do ALL elements satisfy the condition
 */
+
+let arr = [0, 1, 2, 3];
+
+//  {#049,2}
+let val = arr.find((num) => num > 1);
+console.log(val); //2
+
+
+//  {#c78,1}
+console.log(arr.some((num) => num > 1));//true
+
+//  {#3ff,2}
+console.log(arr.every((num) => num > 1));
+//false
